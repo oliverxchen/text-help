986763b38a6ba8df35c29732547292f9847d4f7e
Ubuntu <ubuntu@ip-172-31-16-134.ap-southeast-1.compute.internal>
2015-08-16 02:45:50 +0000

    kinda working

--- index.js
+++ index.js
@@ -39,13 +39,24 @@ app.get('/new', function (res, res) {
       if (err){
           return console.error('could not connect to postgres',err);
       }
-      client.query('select phone_number from raw_conversation where is_sent = false order by timestamp_local desc',function(err,result){
+      client.query('select request_id, phone_number, sms_content from raw_conversation where is_sent = false order by timestamp_local desc',function(err,result){
           if (err){
               return console.error('error running query',err);
           }
           res.writeHead(200,{"Content-Type":'application/json'});
-          res.end(JSON.stringify({"payload":{"task": "send","secret": "secret_key","messages":[{"to": '+' + result.rows[0].phone_number.toString(),"message": "the message goes here","uuid": "aada21b0-0615-4957-bcb3"}]}}));
-    });
+
+          if (result.rows[0] !== undefined) {
+              var output = JSON.stringify({"payload":{"task": "send","secret": "secret_key","messages":[{"to": result.rows[0].phone_number.toString(),"message": result.rows[0].sms_content, "uuid": "aada21b0-0615-4957-bcb3"}]}});
+              console.log(output);
+              res.end(output);
+              
+              client.query('update raw_conversation set is_sent = true where request_id = ' + result.rows[0].request_id, function(err,result) {
+              });
+          }
+          else {
+              res.end('');
+          }
+      });
   });
 });
 
@@ -54,10 +65,31 @@ var message;
 
 app.post('/new', function(req, res) {
     message = req.body.message;
-    query_insert = req.body.from + ',' + 'to_timestamp(' + req.body.sent_timestamp/1000 + '),\'' + req.body.message + '\'';  
+    query_insert = '\'' + req.body.from + '\',' + 'to_timestamp(' + req.body.sent_timestamp/1000 + '),\'' + req.body.message + '\'';  
     //console.log(query_insert);
 
-    io.emit('chat message', req.body.message);
+    console.log(message);
+    var client = new pg.Client(conString);
+    client.connect(function(err, req) {
+        if(err) {
+            return console.error('could not connect to postgres', err);
+        }
+
+        var query_check = 'SELECT COUNT(*) FROM raw_conversation WHERE is_sent = true and sms_content = \'' + message + '\'';
+
+        client.query(query_check, function(err, result) {
+            if(err) {
+                return console.error('error running query check', err);
+            }
+
+            console.log(result.rows[0].count);
+            if (result.rows[0].count < 2) {
+                console.log('in the if');
+                io.emit('chat message', message);
+            }
+            client.end();
+        });
+    });
 
 
     var client = new pg.Client(conString);
@@ -65,11 +97,11 @@ app.post('/new', function(req, res) {
         if(err) {
             return console.error('could not connect to postgres', err);
         }
-        query_insert = 'INSERT INTO raw_conversation SELECT (\'Victim1\',\'Volunteer1\',' + query_insert + ', false) WHERE \'' + message + '\' NOT IN (SELECT sms_content from raw_conversation)';
-        //console.log(query_insert);
-        client.query(query_insert, function(err, result) {
+        query_complete = 'INSERT INTO raw_conversation(\"from\", \"to\", phone_number, timestamp_local, sms_content, is_sent) SELECT \'Victim1\',\'Volunteer1\',' + query_insert + ', true WHERE \'' + message + '\' NOT IN (SELECT sms_content from raw_conversation)';
+        console.log(query_complete);
+        client.query(query_complete, function(err, result) {
             if(err) {
-                return console.error('error running insert query', err);
+                return console.error('error 1 running insert query', err);
             }
             res.writeHead(200,{"Content-Type":'application/json'});
             res.end("success");
@@ -81,6 +113,21 @@ app.post('/new', function(req, res) {
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
+        var client = new pg.Client(conString);
+        client.connect(function(err, req) {
+            if(err) {
+                return console.error('could not connect to postgres', err);
+            }
+            query_insert = 'INSERT INTO raw_conversation VALUES (\'Volunteer1\',\'Victim1\', \'+6596837238\', NOW(),\'' + msg + '\', false)'; 
+            console.log(query_insert);
+
+            client.query(query_insert, function(err, result) {
+                if(err) {
+                    return console.error('error running insert query', err);
+                }
+                client.end();
+            });
+        });
     });
 });
 
